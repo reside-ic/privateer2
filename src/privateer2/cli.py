57f0b3164b9f9d3e4716548ae83f25f05d6e7567
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-10-16 16:52:15 +0100

    Tidy up

--- src/privateer2/cli.py
+++ src/privateer2/cli.py
@@ -1,18 +1,21 @@
 """Usage:
   privateer2 --version
+  privateer2 [-f=PATH] pull
   privateer2 [-f=PATH] keygen <name>
   privateer2 [-f=PATH] configure <name>
   privateer2 [-f=PATH] check <name>
-  privateer2 [-f=PATH] pull
   privateer2 [-f=PATH] serve [--dry-run] <name>
-  privateer2 [-f=PATH] backup [--dry-run] <name>
-  privateer2 [-f=PATH] restore [--dry-run] <name>
+  privateer2 [-f=PATH] backup [--dry-run] <name> <volume>
+  privateer2 [-f=PATH] restore [--dry-run] <name> <volume> --from=
 
 Options:
   -f=PATH    The path to the privateer configuration [default: privateer.json].
   --server   The name of the server to back up to, if more than one configured
-  --include  Volumes to include in the backup
-  --exclude  Volumes to exclude from the backup
+
+Commentary:
+  In all the above '<name>' refers to the name of the client or server
+  being acted on; the machine we are generating keys for, configuring,
+  checking, serving, backing up from or restoring to.
 """
 
 import docker
@@ -25,6 +28,15 @@ from privateer2.config import read_config
 from privateer2.keys import check, configure, keygen
 from privateer2.restore import restore
 from privateer2.server import serve
+
+
+def pull(cfg):
+    img = [f"mrcide/privateer-client:{cfg.tag}",
+           f"mrcide/privateer-server:{cfg.tag}"]
+    cl = docker.from_env()
+    for nm in img:
+        print(f"pulling '{nm}'")
+        cl.images.pull(nm)
 
 
 def main(argv=None):
@@ -47,9 +59,4 @@ def main(argv=None):
     elif opts["restore"]:
         restore(cfg, opts["<name>"], dry_run=dry_run)
     elif opts["pull"]:
-        img = [f"mrcide/privateer-client:{cfg.tag}",
-               f"mrcide/privateer-server:{cfg.tag}"]
-        cl = docker.from_env()
-        for nm in img:
-            print(f"pulling '{nm}'")
-            cl.images.pull(nm)
+        pull(cfg)
