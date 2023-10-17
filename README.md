bd3a78ec25cadca5229a0f820e437df2ebe41d04
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-10-17 10:25:37 +0100

    Start moving to having an identity file

--- README.md
+++ README.md
@@ -5,10 +5,49 @@
 
 -----
 
-**Table of Contents**
+## The idea
 
-- [Installation](#installation)
-- [License](#license)
+We need a way of syncronising some docker volumes from a machine to some backup server, incrementally, using `rsync`. We previously used [`offen/docker-volume-backup`](https://github.com/offen/docker-volume-backup) to backup volumes in their entirity to another machine as a tar file but the space and time requirements made this hard to use in practice.
+
+### The setup
+
+We assume some number of **server** machines -- these will recieve data, and some number of **client** machines -- these will send data to the server(s).  A client can back any number of volumes to any number of servers, and a server can recieve and serve any unmber of volumes to any number of clients.
+
+A typical topolgy for us would be that we would have a "production" machine which is backing up to one or more servers, and then some additional set of "staging" machines that recieve data from the servers, but which in practice never send any data.
+
+Because we are going to use ssh for transport, we assume existance of [HashiCorp Vault](https://www.vaultproject.io/) to store secrets.
+
+### Configuration
+
+The system is configured via a single `json` document, `privateer.json` which contains information about all the moving parts: servers, clients, volumes and the vault configuration. See [`example/`](example/) for some examples.
+
+We imagine that your configuration will exist in some repo, and that that repo will be checked out on all involved machines. Please add `.privateer_identity` to your `.gitignore` for this repo.
+
+### Setup
+
+After writing a configuration, on any machine run
+
+```
+privateer2 keygen --all
+```
+
+which will generate ssh keypairs for all machines and put them in the vault.
+Then, on each machine run
+
+
+```
+privateer2 configure <name>
+```
+
+replacing `<name>` with the name of the machine within either the `servers` or `clients` section of your configuration.  This sets up a special docker volume that will persist ssh keys and configurations so that communication between clients and servers is straightforward and secure.
+
+You can run
+
+```
+privateer2 status
+```
+
+which prints information about the current setup.
 
 ## Installation
 
--- src/privateer2/cli.py
+++ src/privateer2/cli.py
@@ -3,27 +3,30 @@
   privateer2 [options] pull
   privateer2 [options] keygen (<name> | --all)
   privateer2 [options] configure <name>
-  privateer2 [options] check <name>
-  privateer2 [options] serve <name>
-  privateer2 [options] backup <name> <volume>
-  privateer2 [options] restore <name> <volume> [--server=NAME] [--source=NAME]
-  privateer2 [options] export <name> <volume> [--to=PATH] [--source=NAME]
+  privateer2 [options] status
+  privateer2 [options] check
+  privateer2 [options] serve
+  privateer2 [options] backup <volume>
+  privateer2 [options] restore <volume> [--server=NAME] [--source=NAME]
+  privateer2 [options] export <volume> [--to=PATH] [--source=NAME]
   privateer2 [--dry-run] import <tarfile> <volume>
 
 Options:
-  -f=PATH    The path to the privateer configuration [default: privateer.json].
-  --dry-run  Do nothing, but print docker commands
+  --path=PATH  The path to the configuration [default: privateer.json].
+  --as=NAME    The machine to run the command as
+  --dry-run    Do nothing, but print docker commands
 
 Commentary:
-  In all the above '<name>' refers to the name of the client or server
-  being acted on; the machine we are generating keys for, configuring,
-  checking, serving, backing up from or restoring to.
+  In all the above '--as' (or <name>) refers to the name of the client
+  or server being acted on; the machine we are generating keys for,
+  configuring, checking, serving, backing up from or restoring to.
 
   Note that the 'import' subcommand is quite different and does not
   interact with the configuration. If 'volume' exists already, it will
   fail, so this is fairly safe.
 """
 
+import os
 import docopt
 
 import docker
@@ -47,51 +50,78 @@ def pull(cfg):
         cl.images.pull(nm)
 
 
+def _dont_use(name, opts, cmd):
+    if opts[name]:
+        msg = f"Don't use '{name}' with '{cmd}'"
+        raise Exception(msg)
+
+
+def _find_identity(name, root_config):
+    if name:
+        return name
+    path_as = os.path.join(root_config, ".privateer_identity")
+    if not os.path.exists(path_as):
+        msg = (
+            "Can't determine identity; did you forget to configure?"
+            "Alternatively, pass '--as=NAME' to this command"
+        )
+        raise Exception(msg)
+    with open(path_as) as f:
+        return path_as.read().strip()
+
+
 def main(argv=None):
     opts = docopt.docopt(__doc__, argv)
     if opts["--version"]:
         return about.__version__
 
     dry_run = opts["--dry-run"]
+    name = opts["--as"]
     if opts["import"]:
+        _dont_use("--as", opts, "import")
+        _dont_use("--path", opts, "import")
         return import_tar(opts["<volume>"], opts["<tarfile>"], dry_run=dry_run)
 
-    path_config = opts["-f"]
+    path_config = opts["--path"]
+    root_config = os.path.dirname(path_config) if path_config else os.getcwd()
     cfg = read_config(path_config)
     if opts["keygen"]:
+        _dont_use("--as", opts, "keygen")
         if opts["--all"]:
             keygen_all(cfg)
         else:
             keygen(cfg, opts["<name>"])
     elif opts["configure"]:
+        _dont_use("--as", opts, "configure")
         configure(cfg, opts["<name>"])
-    elif opts["check"]:
-        check(cfg, opts["<name>"])
-    elif opts["serve"]:
-        serve(cfg, opts["<name>"], dry_run=dry_run)
-    elif opts["backup"]:
-        backup(cfg, opts["<name>"], opts["<volume>"], dry_run=dry_run)
-    elif opts["restore"]:
-        restore(
-            cfg,
-            opts["<name>"],
-            opts["<volume>"],
-            server=opts["--server"],
-            source=opts["--source"],
-            dry_run=dry_run,
-        )
-    elif opts["export"]:
-        export_tar(
-            cfg,
-            opts["<name>"],
-            opts["<volume>"],
-            to=opts["--to"],
-            source=opts["--source"],
-            dry_run=dry_run,
-        )
-    elif opts["import"]:
-        import_tar(
-            opts["<name>"], opts["<volume>"], opts["<tarfile>"], dry_run=dry_run
-        )
+        with open(os.path.join(root_config, ".privateer_identity"), "w") as f:
+            f.write(f"{name}\n")
     elif opts["pull"]:
+        _dont_use("--as", opts, "configure")
         pull(cfg)
+    else:
+        name = _find_identity(opts["--as"], root_config)
+        if opts["check"]:
+            check(cfg, name)
+        elif opts["serve"]:
+            serve(cfg, name, dry_run=dry_run)
+        elif opts["backup"]:
+            backup(cfg, name, opts["<volume>"], dry_run=dry_run)
+        elif opts["restore"]:
+            restore(
+                cfg,
+                name,
+                opts["<volume>"],
+                server=opts["--server"],
+                source=opts["--source"],
+                dry_run=dry_run,
+            )
+        elif opts["export"]:
+            export_tar(
+                cfg,
+                name,
+                opts["<volume>"],
+                to=opts["--to"],
+                source=opts["--source"],
+                dry_run=dry_run,
+            )
